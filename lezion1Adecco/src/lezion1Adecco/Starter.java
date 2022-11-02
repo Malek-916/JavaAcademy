f50773260050e9c216a6bde4a2174b6a79027d62
JavaDev <116810620+Malek-916@users.noreply.github.com>
2022-11-02 09:31:38 +0100

    Add files via upload

---
+++ lezion1Adecco/src/lezion1Adecco/Starter.java
@@ -0,0 +1,61 @@
+package lezion1Adecco;
+
+public class Starter {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("********************");
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println("********************");
+		
+		int i = 9;
+		
+		System.out.println("Numero intero: "+ i);
+		
+		/* Come si possono rappresentare i num interi:
+		 * byte, short, int, long
+		 *  
+		 *  
+		 *  byte = -128 ==> +127
+		 *  95% delle volte useremo int.
+		 * 
+		 *  
+		 *  come si possono rappresentare i num decimali:
+		 *  float ==> double
+		 *  
+		 */
+		
+		float restoCoffee = 12.43f;
+		double myDoobie = 12.3;
+		
+		System.out.println("float = " + restoCoffee);
+		
+		/*
+		 * Boolean 
+		 * True or false;
+		 * 
+		 * per convenzione si scrive "is" prima del nome della variabile, es. isRainingToday
+		 * 
+		 * 
+		 * 
+		 */
+		boolean result = 9<2;
+		System.out.println("boolean di 9 < 2 " + result);
+		/*
+		 * char: contiene un carattere (solo uno)
+		 * 
+		 * 
+		 */
+		
+		char myChar = 'Q';
+		System.out.println("Stampa char = " + myChar);
+		
+		
+		
+		
+	}
+
+}
---
+++ lezion1Adecco/src/lezion1Adecco/Starter2.java
@@ -0,0 +1,35 @@
+package lezion1Adecco;
+
+public class Starter2 {
+	
+	public static void main(String[] args) {
+		
+		int i = 0;
+		short s = 1;
+		long l = 11;
+		byte b = 12;
+		
+		float  f = 0.212f;
+		double d = 0.12;
+		
+		char c = '♥';
+		
+		boolean b2 = false;
+		
+		System.out.println("int = " + i+ "\n" +
+				"short = " + s+ "\n" +
+				"long = " + l+ "\n" +
+				"byte = " + b+ "\n" +
+				"float = " + f+ "\n" +
+				"double = " + d+ "\n" +
+				"char = " + c+ "\n" +
+				"int = " + b2+ "\n");
+		
+		 
+		
+		
+		
+
+	}
+
+}
